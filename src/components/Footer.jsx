9f9673ef02095c8910acd909d395cccf7ec5892c
José Inacio <inaciojosea13@gmail.com>
2024-12-10 18:18:40 +0100

    error page y footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import "./Footer.css"
 function Footer (){
 
     return (
-        <></>
+        <>
+            <footer>
+                <a href="https://github.com/tmartin87/BOOKS-APP.git">Books app Github</a>
+            </footer>
+        </>
     )
 }
 
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,10 @@ import "./ErrorPage.css"
 function ErrorPage(){
 
     return (
-        <></>
+        <>
+            <h1 className="error-page">Page not found</h1>
+            <img className="error-page-img" src="/public/a-guideline-on-how-to-fix-error-404-not-found-effectively-519451.png" alt="Error Page" />
+        </>
     )
 }
 
