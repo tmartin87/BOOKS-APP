794f7ca9b62516f55cc9a17a337e2055987405e6
Tomás Martín <tmartindsgn@gmail.com>
2024-12-21 13:10:51 +0100

    buttons&styles

---
+++ src/components/AddToListButton.jsx
@@ -0,0 +1,36 @@
+import listWithCheck from "../assets/listWithCheck.svg";
+import listWithCross from "../assets/listWithCross.svg";
+import { addToList, removeFromList } from "../helperFunctions/updateUserLists.js";
+import { getBooksToRead } from "../helperFunctions/getDataFromDB.js";
+
+function AddToListButton({ book, booksToRead, setBooksToRead }) {
+  return booksToRead && booksToRead.includes(book.id) ? (
+    <div className="AllBooksListRow-options">
+      <div className="AllBooksListRow-icon-wrapper">
+        <img
+          className="AllBooksListRow-icon"
+          src={listWithCross}
+          onClick={() => {
+            removeFromList(book, getBooksToRead, setBooksToRead);
+          }}
+        />
+      </div>
+      <p className="AllBooksListRow-label">Remove</p>
+    </div>
+  ) : (
+    <div className="AllBooksListRow-options">
+      <div className="AllBooksListRow-icon-wrapper">
+        <img
+          className="AllBooksListRow-icon"
+          src={listWithCheck}
+          onClick={() => {
+            addToList(book, getBooksToRead, setBooksToRead);
+          }}
+        />
+      </div>
+      <p className="AllBooksListRow-label">Add to list</p>
+    </div>
+  );
+}
+
+export default AddToListButton;
