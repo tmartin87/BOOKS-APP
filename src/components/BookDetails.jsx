d544e13a3b5ef547f1ce716c99e4667033efc098
Tomás Martín <tmartindsgn@gmail.com>
2025-01-02 20:09:30 +0100

    book_details_1

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -2,14 +2,19 @@ import "./BookDetails.css";
 
 function BookDetails({ book, bookCover }) {
   return (
-    <div className="BookDetails-container">
+    <div className="BookDetails-container">.
+    <h1>BOOK DETAILS</h1>
+    <div className="BookDetails-subcontainer">
+    <img className="BookDetails-cover"src={bookCover || "default_cover.jpg"} alt={`${book.title} cover`} />
+    <div className="BookDetails-text">
       <h2>{book.title}</h2>
       <p><strong>Author:</strong> {book.author}</p>
       <p><strong>Genres:</strong> {book.genres}</p>
       <p><strong>Year:</strong> {book.year}</p>
       <p><strong>Pages:</strong> {book.pages}</p>
       <p><strong>Rating:</strong> {book.rating}</p>
-      <img className="BookDetails-cover"src={bookCover || "default_cover.jpg"} alt={`${book.title} cover`} />
+      </div>
+    </div>
     </div>
   );
 }
