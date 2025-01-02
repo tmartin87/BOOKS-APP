e882558212a774f03a16ba3e61a41efdada9d6da
Tomás Martín <tmartindsgn@gmail.com>
2025-01-02 21:01:14 +0100

    book_details_1

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -8,7 +8,7 @@ function BookDetails({ book, bookCover }) {
     <img className="BookDetails-cover"src={bookCover || "default_cover.jpg"} alt={`${book.title} cover`} />
     <div className="BookDetails-text">
       <h2>{book.title}</h2>
-      <p><strong>Author:</strong> {book.author}</p>
+    <h3><strong>Author:</strong> {book.author}</h3>
       <p><strong>Genres:</strong> {book.genres}</p>
       <p><strong>Year:</strong> {book.year}</p>
       <p><strong>Pages:</strong> {book.pages}</p>
