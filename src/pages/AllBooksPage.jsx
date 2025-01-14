faca05a67e23620842f8e0bab92eb7d136193689
marshall-bits <kz_@hotmail.es>
2025-01-14 11:54:19 +0100

    add feedback

--- src/pages/AllBooksPage.jsx
+++ src/pages/AllBooksPage.jsx
@@ -10,4 +10,6 @@ function AllBooksPage(){
     )
 }
 
+// El componente AllBooksList se usa solamente aquí. ¿Es necesario crear un componente para ello? Si en un futuro necesitamos reutilizarlo en otro lugar lo veo coherente, pero dado que no es el caso, ¿no sería mejor incluirlo directamente en AllBooksPage.jsx?
+
 export default AllBooksPage;
